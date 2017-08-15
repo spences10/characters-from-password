14ed3224fdce16ae40d461ef76eb507e5509db7d
spences10 <spences10apps@gmail.com>
2017-08-15 18:39:23 +0100

    add input

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <input type='password'/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
