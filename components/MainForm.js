503f111a91444f1ff817429d40ca518ba20a4e08
spences10 <spences10apps@gmail.com>
2017-09-17 21:27:22 +0100

    add mian form to contain controls

---
+++ components/MainForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from './Layout'
+
+class MainForm extends React.Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <form>
+        <Layout />>
+      </form>
+    )
+  }
+}
+
+export default MainForm
