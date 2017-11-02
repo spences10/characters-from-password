71e93685e1bdd55b7b25cc350fa763cb5c431240
spences10 <spences10apps@gmail.com>
2017-11-02 12:00:34 +0000

    display char in label

--- components/From.js
+++ components/From.js
@@ -3,40 +3,69 @@ import React from 'react'
 export default class Form extends React.Component {
   state = {
     password: '',
-    selected: 0
+    selected: 0,
+    character: ''
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('====================')
+    console.log(nextProps)
+    console.log('====================')
+    console.log('====================')
+    console.log(nextState)
+    console.log('====================')
   }
 
   render(){
     // make variable to store the options in from <input>
     const items = [...this.state.password]
 
+    // map over contents of items to make <options>
+    // use index for unique key and also option
     const list = items.map((item, index) => {
       // console.log('====================')
       // console.log(item)
       // console.log('====================')
-      return (
-        <option key={index}>{index}</option>
-      )
+      // if (index===0) {
+      //   return (
+      //     <option key={index}>Select Character</option>
+      //   )
+      // } else {
+        return (
+          <option 
+            key={index}
+            letter={item}
+          >
+            {index}
+          </option>
+        )
+      // }
     })
-    
+
     // console.log('====================')
     // console.log(items)
     // console.log('====================')
 
+    const characterFromPassword = this.state.password.charAt(this.state.selected) 
+    // characterFromPassword = characterFromPassword.charAt(this.state.selected)
+
     return (
-      <form>
-        <input 
-          type='password'
-          placeholder='Pasta password here'
-          value={this.state.password}
-          onChange={e => this.setState({password: e.target.value})}
-        />
-        <select
-          onChange={e => this.setState({selected: e.target.value})}
-        >
-          {list}
-        </select>
-      </form>
+      <div>
+        <form>
+          <input 
+            type='password'
+            placeholder='Pasta password here'
+            value={this.state.password}
+            onChange={e => this.setState({password: e.target.value})}
+          />
+          <select
+            onChange={e => this.setState({selected: e.target.value})}
+          >
+            {list}
+          </select>
+        </form>
+        <label>{characterFromPassword}</label>
+      </div>
     )
   }
 }
