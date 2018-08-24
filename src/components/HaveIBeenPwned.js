23b33a54d2ead3c84903cf833a2ff129e1f0af03
spences10 <spences10apps@gmail.com>
2018-08-24 07:22:06 +0100

    add state

--- src/components/HaveIBeenPwned.js
+++ src/components/HaveIBeenPwned.js
@@ -2,11 +2,24 @@ import React from 'react'
 import hash from 'sha1'
 
 class HaveIBeenPwned extends React.Component {
+  state = {
+    sha1: null,
+    firstFiveHashChars: '',
+    restHashChars: ''
+  }
+
+  componentDidUpdate() {
+    this.setState({ sha1: hash(this.props.password) })
+  }
+  componentDidMount() {}
+
+  componentWillReceiveProps() {}
+
   render() {
     return (
       <React.Fragment>
-        <p>{this.props.password}</p>
-        <p>{hash(this.props.password)}</p>
+        <p>{this.sha1}</p>
+        {/* <p>{hash(this.props.password)}</p> */}
       </React.Fragment>
     )
   }
