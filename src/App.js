3f107ae73e8c0f3e778c5b87a8c082c1bf11c5b5
spences10 <spences10apps@gmail.com>
2018-08-24 06:36:21 +0100

    appease lint gods

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+// import logo from './logo.svg'
 import './App.css'
 
 import MainForm from './components/MainForm'
 // import { theme } from '../theme/globalStyle';
-
-import { sha1 } from './helpers'
-
-console.log('=====================')
-console.log(sha1('hello'))
-console.log('=====================')
 
 class App extends Component {
   render() {
