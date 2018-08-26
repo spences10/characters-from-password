778ef4c08cf6ed1213d18c6095affe205dab7836
spences10 <spences10apps@gmail.com>
2018-08-26 09:17:45 +0100

    remove unused files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-// import logo from './logo.svg'
-import './App.css'
 
 import MainForm from './components/MainForm'
 // import { theme } from '../theme/globalStyle';
