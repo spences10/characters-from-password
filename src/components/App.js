35ba4332fba9675faca2682efd4f11758be2d3d4
spences10 <spences10apps@gmail.com>
2018-08-26 10:32:33 +0100

    try to even with context

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import MainForm from './MainForm'
+import HaveIBeenPwned from './HaveIBeenPwned'
 // import { theme } from '../theme/globalStyle';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <MainForm />
+        <HaveIBeenPwned />
       </React.Fragment>
     )
   }
