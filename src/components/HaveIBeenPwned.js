6bcf0ecb94ce4b78bb84f63a7330c27353c1affb
spences10 <spences10apps@gmail.com>
2018-08-26 10:31:38 +0100

    add methods

--- src/components/HaveIBeenPwned.js
+++ src/components/HaveIBeenPwned.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import hash from 'sha1';
+import React from 'react'
+import hash from 'sha1'
 
-import { Dump } from '../helpers';
+import { Dump } from '../helpers'
 
 class HaveIBeenPwned extends React.Component {
   getHash(stringToHash) {
-    return hash(stringToHash);
+    return hash(stringToHash)
   }
+
+  getHIBPHashes() {}
 
   render() {
     return (
@@ -25,8 +27,8 @@ class HaveIBeenPwned extends React.Component {
         />
         {/* <p>{hash(this.props.password)}</p> */}
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default HaveIBeenPwned;
+export default HaveIBeenPwned
