fb9b5e30dfe0d3ad293a120c9d8441de0d921812
spences10 <spences10apps@gmail.com>
2018-08-26 09:23:24 +0100

    unused comments

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -161,30 +161,16 @@ class MainForm extends React.Component {
     // map over contents of items to make <options>
     // use index for unique key and also option
     const list = items.map((item, index) => {
-      // console.log('====================')
-      // console.log(item)
-      // console.log('====================')
-      // if (index===0) {
-      //   return (
-      //     <option key={index}>Select Character</option>
-      //   )
-      // } else {
       return (
         <option key={index} letter={item}>
           {index + 1}
         </option>
       )
-      // }
     })
-
-    // console.log('====================')
-    // console.log(items)
-    // console.log('====================')
 
     const characterFromPassword = this.state.password.charAt(
       this.state.selected - 1
     )
-    // characterFromPassword = characterFromPassword.charAt(this.state.selected)
 
     return (
       <Container>
