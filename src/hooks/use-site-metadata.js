fc6f35e89ff1d8ec4e45a4a576445ffc3313dc0e
spences10 <spences10apps@gmail.com>
2020-03-29 11:01:04 +0100

    ass react seo component and site metadata hook

---
+++ src/hooks/use-site-metadata.js
@@ -0,0 +1,23 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+export const useSiteMetadata = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SITE_METADATA_QUERY {
+        site {
+          siteMetadata {
+            description
+            title
+            image
+            siteUrl
+            siteLanguage
+            siteLocale
+            twitterUsername
+            authorName
+          }
+        }
+      }
+    `
+  );
+  return site.siteMetadata;
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,69 @@
 import React from 'react';
+import SEO from 'react-seo-component';
 import HaveIBeenPwned from '../components/HaveIBeenPwned';
 import MainForm from '../components/MainForm';
 import {
   PasswordContext,
   PasswordProvider,
 } from '../contexts/PasswordContext';
+import { useSiteMetadata } from '../hooks/use-site-metadata';
 import { GlobalStyle } from '../theme/global-style';
 
-// import { theme } from '../theme/globalStyle';
-export default () => (
-  <PasswordProvider>
-    <PasswordContext.Consumer>
-      {({
-        password,
-        selected,
-        handlePasswordChange,
-        handleSelectedChange,
-        passwordHash,
-        sha1,
-        firstFiveHashChars,
-        restHashChars,
-        hibpHashes,
-        getHash,
-      }) => (
-        <>
-          <GlobalStyle />
-          <MainForm
-            password={password}
-            selected={selected}
-            handlePasswordChange={handlePasswordChange}
-            handleSelectedChange={handleSelectedChange}
-          />
-          <HaveIBeenPwned
-            password={password}
-            passwordHash={passwordHash}
-            sha1={sha1}
-            firstFiveHashChars={firstFiveHashChars}
-            restHashChars={restHashChars}
-            hibpHashes={hibpHashes}
-            getHash={getHash}
-          />
-        </>
-      )}
-    </PasswordContext.Consumer>
-  </PasswordProvider>
-);
+export default () => {
+  const {
+    description,
+    title,
+    image,
+    siteUrl,
+    siteLanguage,
+    siteLocale,
+    twitterUsername,
+  } = useSiteMetadata();
+  return (
+    <PasswordProvider>
+      <PasswordContext.Consumer>
+        {({
+          password,
+          selected,
+          handlePasswordChange,
+          handleSelectedChange,
+          passwordHash,
+          sha1,
+          firstFiveHashChars,
+          restHashChars,
+          hibpHashes,
+          getHash,
+        }) => (
+          <>
+            <GlobalStyle />
+            <SEO
+              title={`Characters from Password`}
+              titleTemplate={title}
+              description={description || `nothin’`}
+              image={`${siteUrl}${image}`}
+              pathname={siteUrl}
+              siteLanguage={siteLanguage}
+              siteLocale={siteLocale}
+              twitterUsername={twitterUsername}
+            />
+            <MainForm
+              password={password}
+              selected={selected}
+              handlePasswordChange={handlePasswordChange}
+              handleSelectedChange={handleSelectedChange}
+            />
+            <HaveIBeenPwned
+              password={password}
+              passwordHash={passwordHash}
+              sha1={sha1}
+              firstFiveHashChars={firstFiveHashChars}
+              restHashChars={restHashChars}
+              hibpHashes={hibpHashes}
+              getHash={getHash}
+            />
+          </>
+        )}
+      </PasswordContext.Consumer>
+    </PasswordProvider>
+  );
+};
