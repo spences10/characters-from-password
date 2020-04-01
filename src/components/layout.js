31fef4ed34db9a74e94100cfbf55e98e1362f177
spences10 <spences10apps@gmail.com>
2020-04-01 08:01:00 +0100

    defaults

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,16 +3,14 @@ import styled from 'styled-components'
 
 const AppStyles = styled.main`
   h1 {
-    font-family: ${({ theme }) => theme.font.sans};
+    font-family: ${({ theme }) => theme.font.serif};
+    font-size: ${({ theme }) => theme.fontSize['4xl']};
   }
   p,
   label,
   input,
   select {
-    font-family: ${({ theme }) => theme.font.serif};
-  }
-  section {
-    font-family: ${({ theme }) => theme.font.monospace};
+    font-family: ${({ theme }) => theme.font.sans};
   }
 `
 
