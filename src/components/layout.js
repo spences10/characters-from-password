a6ea9d13f38aab07c56e896c3144d18d475fc7e5
spences10 <spences10apps@gmail.com>
2020-04-03 08:04:35 +0100

    moar headings

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,27 @@ import styled from 'styled-components'
 const AppStyles = styled.main`
   h1 {
     font-family: ${({ theme }) => theme.font.serif};
+    font-size: ${({ theme }) => theme.fontSize['5xl']};
+  }
+  h2 {
+    font-family: ${({ theme }) => theme.font.serif};
     font-size: ${({ theme }) => theme.fontSize['4xl']};
+  }
+  h3 {
+    font-family: ${({ theme }) => theme.font.serif};
+    font-size: ${({ theme }) => theme.fontSize['3xl']};
+  }
+  h4 {
+    font-family: ${({ theme }) => theme.font.serif};
+    font-size: ${({ theme }) => theme.fontSize['2xl']};
+  }
+  h5 {
+    font-family: ${({ theme }) => theme.font.serif};
+    font-size: ${({ theme }) => theme.fontSize.xl};
+  }
+  h6 {
+    font-family: ${({ theme }) => theme.font.serif};
+    font-size: ${({ theme }) => theme.fontSize.lg};
   }
   p,
   label,
