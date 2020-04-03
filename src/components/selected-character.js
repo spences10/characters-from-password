19ecb03d5ae31fd6a1db18cc38a1d261a8f62708
spences10 <spences10apps@gmail.com>
2020-04-03 08:04:29 +0100

    return character type

--- src/components/selected-character.js
+++ src/components/selected-character.js
@@ -4,7 +4,13 @@ export const SelectedCharacter = ({ char }) => {
   function whatIsIt() {
     const asciiNumber = char.charCodeAt(0)
 
-    return String.fromCharCode(asciiNumber)
+    if (asciiNumber > 47 && asciiNumber < 58) return `numerical`
+
+    if (asciiNumber > 64 && asciiNumber < 91) return `uppercase`
+
+    if (asciiNumber > 96 && asciiNumber < 123) return `lowercase`
+
+    return `a symbol`
   }
 
   return (
@@ -12,6 +18,7 @@ export const SelectedCharacter = ({ char }) => {
       <span>"</span>
       {char}
       <span>"</span>
+      {char && <h2>This character is {whatIsIt()}</h2>}
     </section>
   )
 }
