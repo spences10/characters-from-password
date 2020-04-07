89b92e1372f7672d9ddcb12a4c4750e20670d94a
spences10 <spences10apps@gmail.com>
2020-04-07 20:07:34 +0100

    more sttyles to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,6 +15,12 @@ const AppStyles = styled.main`
       box-shadow: ${({ theme }) => theme.boxShadow.outline};
     }
   }
+  fieldset {
+    text-align: center;
+    select {
+      margin-left: ${({ theme }) => theme.spacing['4']};
+    }
+  }
   h1 {
     font-size: ${({ theme }) => theme.fontSize['4xl']};
     font-family: ${({ theme }) => theme.font.serif};
@@ -26,6 +32,17 @@ const AppStyles = styled.main`
     font-family: ${({ theme }) => theme.font.serif};
     margin-top: ${({ theme }) => theme.spacing[8]};
     line-height: ${({ theme }) => theme.lineHeight.none};
+  }
+  input {
+    text-align: center;
+    border-radius: ${({ theme }) => theme.borderRadius.full};
+    border-style: none;
+    border: solid 1px ${({ theme }) => theme.colours.grey[500]};
+    padding: ${({ theme }) => theme.spacing[3]};
+    min-height: ${({ theme }) => theme.spacing['20']};
+    font-size: ${({ theme }) => theme.fontSize['5xl']};
+    margin-left: -${({ theme }) => theme.spacing['64']};
+    margin-right: -${({ theme }) => theme.spacing['64']};
   }
   p {
     font-size: ${({ theme }) => theme.fontSize.base};
@@ -41,14 +58,16 @@ const AppStyles = styled.main`
     }
     word-break: break-word;
   }
-  input {
-    text-align: center;
-    border-radius: ${({ theme }) => theme.borderRadius.full};
-    border-style: none;
-    border: solid 1px ${({ theme }) => theme.colours.grey[500]};
-    padding: ${({ theme }) => theme.spacing[3]};
-    margin-bottom: ${({ theme }) => theme.spacing[3]};
-    width: 100%;
+  ul {
+    margin-top: 30px;
+    margin-top: ${({ theme }) => theme.spacing[3]};
+    li {
+      list-style-type: circle;
+      margin-left: ${({ theme }) => theme.spacing['8']};
+      p {
+        margin-top: 10px;
+      }
+    }
   }
   ${down('sm')} {
     background-color: seagreen;
