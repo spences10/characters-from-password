8c22f1d8b53fb41c91ea08176405ff9924f682e0
spences10 <spences10apps@gmail.com>
2020-04-08 09:42:53 +0100

    move picker out from hibp check

--- src/components/character-picker.js
+++ src/components/character-picker.js
@@ -1,54 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import hash from 'sha1'
-import { fetchHIBPData } from '../helpers'
+import React, { useState } from 'react'
 import { NumberOfBreaches } from './number-of-breaches'
 import { SelectedCharacter } from './selected-character'
 
-const apiUrl = `https://api.pwnedpasswords.com/range/`
-
-export const CharacterPicker = () => {
-  const [hashes, setHashes] = useState(``)
-  const [getSha1, setSha1] = useState(``)
-  const [getPassword, setPassword] = useState(``)
+export const CharacterPicker = ({ password, breaches }) => {
   const [selected, setSelected] = useState(``)
 
-  useEffect(() => {
-    function getHIBPHashes(apiUrl, sha1) {
-      if (sha1.length < 5) return null
-      fetchHIBPData(`${apiUrl}/${sha1.substring(0, 5)}`).then(
-        result => {
-          setHashes(result.data.split('\r\n'))
-        }
-      )
-    }
-    getHIBPHashes(apiUrl, getSha1)
-  }, [getSha1])
-
-  function handleInputChange(e) {
-    const password = e.target.value
-    setSha1(hash(password).toUpperCase())
-    setPassword(password)
-  }
-
-  function getNumberOfBreaches() {
-    if (!hashes) return 0
-    const match = hashes.filter(
-      hash =>
-        hash.substring(0, hash.indexOf(':')) ===
-        getSha1.slice(5, getSha1.length)
-    )
-
-    const breaches = match[0]
-    if (!breaches) return 0
-    const numberOfBreaches = breaches.substring(
-      breaches.indexOf(`:`) + 1,
-      breaches.length
-    )
-    return numberOfBreaches
-  }
-
-  const items = [...getPassword]
-  const characterFromPassword = getPassword.charAt(selected - 1)
+  const items = [...password]
+  const characterFromPassword = password.charAt(selected - 1)
 
   function handleSelectedChange(e) {
     setSelected(e.target.value)
@@ -63,37 +21,19 @@ export const CharacterPicker = () => {
   })
 
   return (
-    <form onClick={e => e.preventDefault()}>
-      <fieldset>
-        <label
-          htmlFor='passwordInput'
-          aria-label='Enter Password You Want to Pick Characters From:'
-          aria-labelledby='passwordInput'
-        />
-        <input
-          id='passwordInput'
-          type='password'
-          placeholder='password me up yo!'
-          value={getPassword}
-          onChange={e => handleInputChange(e)}
-        />
-      </fieldset>
-      <fieldset>
-        <label htmlFor='characterFromPassword'>
-          Select Character:
-        </label>
-        <select
-          id='characterFromPassword'
-          onChange={e => handleSelectedChange(e)}
-        >
-          {list}
-        </select>
-        <SelectedCharacter char={characterFromPassword} />
-        <NumberOfBreaches
-          breaches={getNumberOfBreaches()}
-          passwordLength={getPassword}
-        />
-      </fieldset>
-    </form>
+    <fieldset>
+      <label htmlFor='characterFromPassword'>Select Character:</label>
+      <select
+        id='characterFromPassword'
+        onChange={e => handleSelectedChange(e)}
+      >
+        {list}
+      </select>
+      <SelectedCharacter char={characterFromPassword} />
+      <NumberOfBreaches
+        breaches={breaches()}
+        passwordLength={password}
+      />
+    </fieldset>
   )
 }
