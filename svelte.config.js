36dc1d00ea50bf2ba21ab743650f2594c6a5beca
spences10 <spences10apps@gmail.com>
2021-10-17 11:32:11 +0100

    feat: :sparkles: add and configure MDSveX

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
+import { mdsvex } from 'mdsvex'
+import mdsvexConfig from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
 import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
+
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
@@ -13,6 +17,7 @@ const config = {
     preprocess({
       postcss: true,
     }),
+    mdsvex(mdsvexConfig),
   ],
 }
 
