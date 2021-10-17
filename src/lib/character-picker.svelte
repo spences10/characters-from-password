743cc733f68fc5528cca207e80615b47c956b857
spences10 <spences10apps@gmail.com>
2021-10-17 13:48:40 +0100

    feat: :sparkles: disable select on no character value

--- src/lib/character-picker.svelte
+++ src/lib/character-picker.svelte
@@ -3,9 +3,9 @@
 
   export let password = ''
   let selectedCharacter = ''
-
   $: items = [...password]
   $: char = password.charAt(selectedCharacter - 1)
+  $: disabled = items.length ? `` : `disabled="disabled"`
 
   const handleChange = e => {
     selectedCharacter = e.target.value
@@ -13,7 +13,12 @@
   }
 </script>
 
-<select on:change={e => handleChange(e)}>
+<span class="all-prose">Select Password Character:</span>
+<select
+  class="select select-bordered select-primary"
+  {disabled}
+  on:change={e => handleChange(e)}
+>
   {#each items as item, index}
     <option key={index} letter={item}>
       {index + 1}
