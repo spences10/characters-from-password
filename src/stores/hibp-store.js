d2c5e6c6e94db7f28f42b7cdf05b3fee22c1aed3
spences10 <spences10apps@gmail.com>
2021-10-17 08:54:08 +0100

    refactor: :recycle: add empty results check

--- src/stores/hibp-store.js
+++ src/stores/hibp-store.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store'
 
-export const breaches = writable(``)
+export const breaches = writable(0)
 export const fetchHIBPHashes = async sha1 => {
   const url = `https://api.pwnedpasswords.com/range/${sha1.substring(
     0,
@@ -11,16 +11,20 @@ export const fetchHIBPHashes = async sha1 => {
 
   // work through hash data
   const splitText = resText.split('\r\n')
+
+  if (!splitText) return breaches.set(0)
   const match = splitText.filter(
     hash =>
       hash.substring(0, hash.indexOf(':')) ===
       sha1.slice(5, sha1.length)
   )
+
   const breachesMatch = match[0]
-  if (!breachesMatch) return 0
+  if (!breachesMatch) return breaches.set(0)
   const numberOfBreaches = breachesMatch.substring(
     breachesMatch.indexOf(`:`) + 1,
     breachesMatch.length
   )
+
   breaches.set(numberOfBreaches)
 }
