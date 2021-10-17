a45cad0fccded987bab8ad72c512410f705e042c
spences10 <spences10apps@gmail.com>
2021-10-17 20:11:30 +0100

    feat: :sparkles: add Fathom Client

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,17 @@
 <script>
   import Footer from '$lib/components/footer.svelte'
   import Nav from '$lib/components/nav.svelte'
+  import * as Fathom from 'fathom-client'
   import { onMount } from 'svelte'
   import { themeChange } from 'theme-change'
   import '../app.css'
 
   onMount(async () => {
     themeChange(false)
+    Fathom.load(import.meta.env.VITE_FATHOM_ID, {
+      url: import.meta.env.VITE_FATHOM_URL,
+      excludedDomains: ['localhost'],
+    })
   })
 </script>
 
