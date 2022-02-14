db1a3cf51495ba5d27ef56de6ffc30164e8a4e9e
spences10 <spences10apps@gmail.com>
2022-02-14 12:04:46 +0000

    chore: :wrench: update config, for kit.target
    update prettier config, ignore build/dev folders

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
-import { mdsvex } from 'mdsvex'
-import mdsvexConfig from './mdsvex.config.js'
-import preprocess from 'svelte-preprocess'
 import vercel from '@sveltejs/adapter-vercel'
+import { mdsvex } from 'mdsvex'
+import preprocess from 'svelte-preprocess'
+import mdsvexConfig from './mdsvex.config.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
 
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
     adapter: vercel(),
   },
 
