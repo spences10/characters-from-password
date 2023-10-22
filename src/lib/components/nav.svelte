09f87e7f3074bd4431056bac51b4eeac402252df
spences10 <spences10apps@gmail.com>
2023-10-22 09:48:34 +0100

    refactor: :lipstick: mobile responsive nav

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -9,13 +9,31 @@
 	]
 </script>
 
-<nav class="navbar shadow-lg bg-neutral text-neutral-content mb-16">
-	<ul
-		class=""
-	>
+<nav
+	class="navbar shadow-lg bg-neutral text-neutral-content mb-16 justify-between"
+>
+	<ul class="hidden md:flex md:flex-row">
 		{#each links as { href, name }}
 			<a {href} class="btn btn-ghost btn-sm rounded-btn">{name}</a>
 		{/each}
 	</ul>
+	<div class="md:hidden md:flex-none md:bg-red-500">
+		<ul class="menu menu-horizontal px-1">
+			<li>
+				<details>
+					<summary>Links</summary>
+					<ul class="p-2 bg-base-100 flex flex-col">
+						{#each links as { href, name }}
+							<li>
+								<a {href} class="">
+									{name}
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</details>
+			</li>
+		</ul>
+	</div>
 	<div class="navbar-end"><ThemeSelect /></div>
 </nav>
